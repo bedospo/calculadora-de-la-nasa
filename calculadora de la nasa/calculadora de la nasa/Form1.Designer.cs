ddb53712f54ebd4849697e85d7da0956831e58cd
miguel hernandez <miguelhesco17@gmail.com>
2019-08-31 09:05:29 -0500

    confirmo

--- calculadora de la nasa/calculadora de la nasa/Form1.Designer.cs
+++ calculadora de la nasa/calculadora de la nasa/Form1.Designer.cs
@@ -28,13 +28,269 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.boteliminar = new System.Windows.Forms.Button();
+            this.pantalla = new System.Windows.Forms.TextBox();
+            this.bot7 = new System.Windows.Forms.Button();
+            this.bot4 = new System.Windows.Forms.Button();
+            this.bot1 = new System.Windows.Forms.Button();
+            this.bot0 = new System.Windows.Forms.Button();
+            this.botmultiplicacion = new System.Windows.Forms.Button();
+            this.bot9 = new System.Windows.Forms.Button();
+            this.bot6 = new System.Windows.Forms.Button();
+            this.bot3 = new System.Windows.Forms.Button();
+            this.botpunto = new System.Windows.Forms.Button();
+            this.botdivision = new System.Windows.Forms.Button();
+            this.bot8 = new System.Windows.Forms.Button();
+            this.bot5 = new System.Windows.Forms.Button();
+            this.bot2 = new System.Windows.Forms.Button();
+            this.botresta = new System.Windows.Forms.Button();
+            this.botsuma = new System.Windows.Forms.Button();
+            this.botraiz = new System.Windows.Forms.Button();
+            this.botigual = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // boteliminar
+            // 
+            this.boteliminar.Location = new System.Drawing.Point(13, 50);
+            this.boteliminar.Name = "boteliminar";
+            this.boteliminar.Size = new System.Drawing.Size(48, 32);
+            this.boteliminar.TabIndex = 0;
+            this.boteliminar.Text = "CE";
+            this.boteliminar.UseVisualStyleBackColor = true;
+            this.boteliminar.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pantalla
+            // 
+            this.pantalla.Location = new System.Drawing.Point(12, 12);
+            this.pantalla.Name = "pantalla";
+            this.pantalla.ReadOnly = true;
+            this.pantalla.Size = new System.Drawing.Size(205, 20);
+            this.pantalla.TabIndex = 1;
+            this.pantalla.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // bot7
+            // 
+            this.bot7.Location = new System.Drawing.Point(12, 88);
+            this.bot7.Name = "bot7";
+            this.bot7.Size = new System.Drawing.Size(48, 32);
+            this.bot7.TabIndex = 0;
+            this.bot7.Text = "7";
+            this.bot7.UseVisualStyleBackColor = true;
+            this.bot7.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // bot4
+            // 
+            this.bot4.Location = new System.Drawing.Point(12, 126);
+            this.bot4.Name = "bot4";
+            this.bot4.Size = new System.Drawing.Size(48, 32);
+            this.bot4.TabIndex = 0;
+            this.bot4.Text = "4";
+            this.bot4.UseVisualStyleBackColor = true;
+            this.bot4.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // bot1
+            // 
+            this.bot1.Location = new System.Drawing.Point(12, 164);
+            this.bot1.Name = "bot1";
+            this.bot1.Size = new System.Drawing.Size(48, 32);
+            this.bot1.TabIndex = 0;
+            this.bot1.Text = "1";
+            this.bot1.UseVisualStyleBackColor = true;
+            this.bot1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // bot0
+            // 
+            this.bot0.Location = new System.Drawing.Point(12, 202);
+            this.bot0.Name = "bot0";
+            this.bot0.Size = new System.Drawing.Size(102, 32);
+            this.bot0.TabIndex = 0;
+            this.bot0.Text = "0";
+            this.bot0.UseVisualStyleBackColor = true;
+            this.bot0.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // botmultiplicacion
+            // 
+            this.botmultiplicacion.Location = new System.Drawing.Point(121, 50);
+            this.botmultiplicacion.Name = "botmultiplicacion";
+            this.botmultiplicacion.Size = new System.Drawing.Size(48, 32);
+            this.botmultiplicacion.TabIndex = 0;
+            this.botmultiplicacion.Text = "*";
+            this.botmultiplicacion.UseVisualStyleBackColor = true;
+            this.botmultiplicacion.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // bot9
+            // 
+            this.bot9.Location = new System.Drawing.Point(121, 88);
+            this.bot9.Name = "bot9";
+            this.bot9.Size = new System.Drawing.Size(48, 32);
+            this.bot9.TabIndex = 0;
+            this.bot9.Text = "9";
+            this.bot9.UseVisualStyleBackColor = true;
+            this.bot9.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // bot6
+            // 
+            this.bot6.Location = new System.Drawing.Point(121, 126);
+            this.bot6.Name = "bot6";
+            this.bot6.Size = new System.Drawing.Size(48, 32);
+            this.bot6.TabIndex = 0;
+            this.bot6.Text = "6";
+            this.bot6.UseVisualStyleBackColor = true;
+            this.bot6.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // bot3
+            // 
+            this.bot3.Location = new System.Drawing.Point(121, 164);
+            this.bot3.Name = "bot3";
+            this.bot3.Size = new System.Drawing.Size(48, 32);
+            this.bot3.TabIndex = 0;
+            this.bot3.Text = "3";
+            this.bot3.UseVisualStyleBackColor = true;
+            this.bot3.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // botpunto
+            // 
+            this.botpunto.Location = new System.Drawing.Point(121, 202);
+            this.botpunto.Name = "botpunto";
+            this.botpunto.Size = new System.Drawing.Size(48, 32);
+            this.botpunto.TabIndex = 0;
+            this.botpunto.Text = ".";
+            this.botpunto.UseVisualStyleBackColor = true;
+            this.botpunto.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // botdivision
+            // 
+            this.botdivision.Location = new System.Drawing.Point(67, 50);
+            this.botdivision.Name = "botdivision";
+            this.botdivision.Size = new System.Drawing.Size(48, 32);
+            this.botdivision.TabIndex = 0;
+            this.botdivision.Text = "/";
+            this.botdivision.UseVisualStyleBackColor = true;
+            this.botdivision.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // bot8
+            // 
+            this.bot8.Location = new System.Drawing.Point(66, 88);
+            this.bot8.Name = "bot8";
+            this.bot8.Size = new System.Drawing.Size(48, 32);
+            this.bot8.TabIndex = 0;
+            this.bot8.Text = "8";
+            this.bot8.UseVisualStyleBackColor = true;
+            this.bot8.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // bot5
+            // 
+            this.bot5.Location = new System.Drawing.Point(66, 126);
+            this.bot5.Name = "bot5";
+            this.bot5.Size = new System.Drawing.Size(48, 32);
+            this.bot5.TabIndex = 0;
+            this.bot5.Text = "5";
+            this.bot5.UseVisualStyleBackColor = true;
+            this.bot5.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // bot2
+            // 
+            this.bot2.Location = new System.Drawing.Point(66, 164);
+            this.bot2.Name = "bot2";
+            this.bot2.Size = new System.Drawing.Size(48, 32);
+            this.bot2.TabIndex = 0;
+            this.bot2.Text = "2";
+            this.bot2.UseVisualStyleBackColor = true;
+            this.bot2.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // botresta
+            // 
+            this.botresta.Location = new System.Drawing.Point(175, 50);
+            this.botresta.Name = "botresta";
+            this.botresta.Size = new System.Drawing.Size(48, 32);
+            this.botresta.TabIndex = 0;
+            this.botresta.Text = "-";
+            this.botresta.UseVisualStyleBackColor = true;
+            this.botresta.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // botsuma
+            // 
+            this.botsuma.Location = new System.Drawing.Point(175, 88);
+            this.botsuma.Name = "botsuma";
+            this.botsuma.Size = new System.Drawing.Size(48, 32);
+            this.botsuma.TabIndex = 0;
+            this.botsuma.Text = "+";
+            this.botsuma.UseVisualStyleBackColor = true;
+            this.botsuma.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // botraiz
+            // 
+            this.botraiz.Location = new System.Drawing.Point(175, 126);
+            this.botraiz.Name = "botraiz";
+            this.botraiz.Size = new System.Drawing.Size(48, 32);
+            this.botraiz.TabIndex = 0;
+            this.botraiz.Text = "Raiz";
+            this.botraiz.UseVisualStyleBackColor = true;
+            this.botraiz.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // botigual
+            // 
+            this.botigual.Location = new System.Drawing.Point(175, 164);
+            this.botigual.Name = "botigual";
+            this.botigual.Size = new System.Drawing.Size(48, 70);
+            this.botigual.TabIndex = 0;
+            this.botigual.Text = "=";
+            this.botigual.UseVisualStyleBackColor = true;
+            this.botigual.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(230, 243);
+            this.Controls.Add(this.pantalla);
+            this.Controls.Add(this.botigual);
+            this.Controls.Add(this.botpunto);
+            this.Controls.Add(this.bot2);
+            this.Controls.Add(this.bot3);
+            this.Controls.Add(this.bot0);
+            this.Controls.Add(this.bot5);
+            this.Controls.Add(this.botraiz);
+            this.Controls.Add(this.bot6);
+            this.Controls.Add(this.bot1);
+            this.Controls.Add(this.bot8);
+            this.Controls.Add(this.botsuma);
+            this.Controls.Add(this.bot9);
+            this.Controls.Add(this.bot4);
+            this.Controls.Add(this.botdivision);
+            this.Controls.Add(this.botresta);
+            this.Controls.Add(this.botmultiplicacion);
+            this.Controls.Add(this.bot7);
+            this.Controls.Add(this.boteliminar);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button boteliminar;
+        private System.Windows.Forms.TextBox pantalla;
+        private System.Windows.Forms.Button bot7;
+        private System.Windows.Forms.Button bot4;
+        private System.Windows.Forms.Button bot1;
+        private System.Windows.Forms.Button bot0;
+        private System.Windows.Forms.Button botmultiplicacion;
+        private System.Windows.Forms.Button bot9;
+        private System.Windows.Forms.Button bot6;
+        private System.Windows.Forms.Button bot3;
+        private System.Windows.Forms.Button botpunto;
+        private System.Windows.Forms.Button botdivision;
+        private System.Windows.Forms.Button bot8;
+        private System.Windows.Forms.Button bot5;
+        private System.Windows.Forms.Button bot2;
+        private System.Windows.Forms.Button botresta;
+        private System.Windows.Forms.Button botsuma;
+        private System.Windows.Forms.Button botraiz;
+        private System.Windows.Forms.Button botigual;
     }
 }
 
--- calculadora de la nasa/calculadora de la nasa/Form1.cs
+++ calculadora de la nasa/calculadora de la nasa/Form1.cs
@@ -16,5 +16,15 @@ namespace calculadora_de_la_nasa
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            pantalla
+        }
     }
 }
